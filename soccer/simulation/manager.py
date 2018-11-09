39abf4bf4bf4b41a6f325432a7b0ab21c69b1e9d
Phillip Nguyen <nh273@cornell.edu>
2018-11-09 18:03:02 -0500

    stumps for manager & player class

---
+++ soccer/simulation/manager.py
@@ -0,0 +1,18 @@
+"""These select the players based on different criteria"""
+
+
+class Manager():
+    """Boiler plate manager with basic functionalities"""
+
+    def __init__(self):
+        self.goalkeeper = []
+        self.defenders = []
+        self.midfielders = []
+        self.forwards = []
+        self.points = {}
+
+    def transfer_in(self, player):
+        pass
+
+    def transfer_out(self, player):
+        pass
